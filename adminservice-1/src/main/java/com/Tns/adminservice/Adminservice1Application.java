e5a80a3a8c35814975ad1d00fbbd40bb828d3a38
kalvi <kalvi@DESKTOP-DVFUUI8>
2023-04-11 13:21:51 +0530

    This is the first commit

---
+++ adminservice-1/src/main/java/com/Tns/adminservice/Adminservice1Application.java
@@ -0,0 +1,13 @@
+package com.Tns.adminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Adminservice1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Adminservice1Application.class, args);
+	}
+
+}
---
+++ adminservice-1/src/test/java/com/Tns/adminservice/Adminservice1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Tns.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Adminservice1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
